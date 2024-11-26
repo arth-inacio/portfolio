a796753fb833b8a8150ff7482c264bb6c06e622b
Arthur <arthur.inacio3@gmail.com>
2024-11-26 17:23:28 -0300

    feat[potfolio]: Criando função js para manipular o menu-hamburguer

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+}
